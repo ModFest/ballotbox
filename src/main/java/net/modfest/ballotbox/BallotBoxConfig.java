a7bffbd58ce8c1c2dfccb53ae73db8c34ef2c48e
sisby-folk <nora.herrah.heartstrings@gmail.com>
2024-12-02 11:08:11 +1100

    capital f

--- src/main/java/net/modfest/ballotbox/BallotBoxConfig.java
+++ src/main/java/net/modfest/ballotbox/BallotBoxConfig.java
@@ -10,7 +10,7 @@ public class BallotBoxConfig extends ReflectiveConfig {
 	@Comment("Whether to replace the bug report button with another link")
 	public final TrackedValue<Boolean> replace_bugs = value(true);
 	@Comment("The text to use to replace the bug report button")
-	public final TrackedValue<String> bug_text = value("Modfest Discord");
+	public final TrackedValue<String> bug_text = value("ModFest Discord");
 	@Comment("The link to use to replace the bug report button")
 	public final TrackedValue<String> bug_url = value("https://discord.gg/gn543Ee");
 	@Comment("Whether to replace the realms button on the title screen with a credits button")
